f6de86c75c387979e2687c30c1eec437ee2f33ef
quysinhdo <quysinh2004@gmail.com>
2024-09-09 11:06:48 +0700

    Ket noi CSDL

---
+++ src/main/java/vn/iotstar/configs/DBConnectSQL.java
@@ -0,0 +1,31 @@
+package vn.iotstar.configs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnectSQL {
+	public static final String serverName = "DESKTOP-IJ7RR4F\\QUYSINH";
+	public static String dbName = "ltwebct4";
+	public static String portNumber = "1433";
+	public static String instance = "";
+	public static String userID = "sa";
+	public static String password = "123456";
+	
+	//Kết nối SQL Server với SQL Authentication
+		public Connection getConnection() throws Exception {
+			String url = "jdbc:sqlserver://" + serverName + "\\" + instance + ":" + portNumber + ";databaseName=" + dbName;
+			if (instance == null || instance.trim().isEmpty())
+				url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			return DriverManager.getConnection(url, userID, password);
+		}
+
+
+			 
+			public static void main(String[] args) {
+				try {
+				System.out.println(new DBConnectSQL().getConnection());
+				} catch (Exception e) { e.printStackTrace(); }
+			
+	    }
+}
---
+++ src/main/java/vn/iotstar/dao/iUserDao.java
@@ -0,0 +1,10 @@
+package vn.iotstar.dao;
+
+import java.util.List;
+import vn.iotstar.models.UserModel;
+
+public interface iUserDao {
+	List<UserModel> findAll();
+	UserModel findAId(int id);
+	void insert(UserModel user);
+}
